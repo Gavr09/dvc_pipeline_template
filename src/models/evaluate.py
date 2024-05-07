766b89a6e7f6fc0dabf57ec5c1d7343fb1fe072f
Gavr09 <ng062974@gmail.com>
2024-05-07 19:20:48 +0300

    Evaluate and train stages were updated

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -2,29 +2,64 @@ import pandas as pd
 import json
 import pickle
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import yaml
+import sys
+import os
 
-X_test_path = '../../data/processed/x_test.csv'
-y_test_path = '../../data/processed/y_test.csv'
-metrics_json = '../../reports/scores.json'
-model_path = '../../models/model.pkl'
+def evaluate(data_path, model_path, output_dir):
+    X_test_path = os.path.join(data_path, 'x_test.csv')
+    y_test_path = os.path.join(data_path, 'y_test.csv')
+    metrics_json = os.path.join(output_dir, 'scores.json')
 
-X_test = pd.read_csv(X_test_path)
-y_test = pd.read_csv(y_test_path)['target'].values
+    X_test = pd.read_csv(X_test_path)
+    y_test = pd.read_csv(y_test_path)['target'].values
 
-with open(model_path,'rb') as fp:
-    model = pickle.load(fp)
+    with open(model_path,'rb') as fp:
+        model = pickle.load(fp)
 
-y_pred = model.predict(X_test)
+    y_pred = model.predict(X_test)
 
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-rmse = mse**0.5
-r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = mse**0.5
+    r2 = r2_score(y_test, y_pred)
 
-metrics_dict = {'mae':mae,
-                'mse':mse,
-                'rmse':rmse,
-                'r2':r2}
+    metrics_dict = {'mae':mae,
+                    'mse':mse,
+                    'rmse':rmse,
+                    'r2':r2}
 
-with open(metrics_json, 'w') as fp:
-    json.dump(metrics_dict, fp)
+    with open(metrics_json, 'w') as fp:
+        json.dump(metrics_dict, fp)
+
+
+def main():
+
+    # get data_path and model_path from sys.argv
+    if len(sys.argv) != 3:
+        sys.stderr.write("Arguments error. Usage:\n")
+        sys.stderr.write("\tpython prepare.py data-file\n")
+        sys.exit(1)
+
+    data_path = sys.argv[1]
+    model_dir = sys.argv[2]
+
+    if not os.path.exists(data_path):
+        sys.stderr.write("data_path is not exist!\n")
+        sys.exit(1)
+
+    if not os.path.exists(model_dir):
+        sys.stderr.write("model_dir is not exist!\n")
+        sys.exit(1)
+
+    model_path = os.path.join(model_dir, 'model.pkl')
+
+    # path for outputs
+    output_dir = './reports/'
+    os.makedirs(output_dir, exist_ok=True)
+
+    evaluate(data_path, model_path, output_dir)
+
+
+if __name__ == "__main__":
+    main()
--- src/models/train.py
+++ src/models/train.py
@@ -1,16 +1,47 @@
 import pandas as pd
 from sklearn.linear_model import Ridge
 import pickle
+import yaml
+import sys
+import os
 
-X_train_path = '../../data/processed/x_train.csv'
-y_train_path = '../../data/processed/y_train.csv'
-ALPHA = 0.5
-model_path = '../../models/model.pkl'
+def train_model(data_path, alpha, output_dir):
+    X_train_path = os.path.join(data_path, 'x_train.csv')
+    y_train_path = os.path.join(data_path, 'y_train.csv')
+    model_path = os.path.join(output_dir, 'model.pkl')
 
-X_train = pd.read_csv(X_train_path)
-y_train = pd.read_csv(y_train_path)['target'].values
+    X_train = pd.read_csv(X_train_path)
+    y_train = pd.read_csv(y_train_path)['target'].values
 
-model = Ridge(alpha=ALPHA)
-model.fit(X_train, y_train)
+    model = Ridge(alpha=alpha)
+    model.fit(X_train, y_train)
 
-pickle.dump(model, open(model_path, 'wb'))
+    pickle.dump(model, open(model_path, 'wb'))
+
+def main():
+
+    # read params from yaml-file
+    params = yaml.safe_load(open("params.yaml"))["train_model"]
+    alpha = params["alpha"]
+
+    # get data_path from sys.argv
+    if len(sys.argv) != 2:
+        sys.stderr.write("Arguments error. Usage:\n")
+        sys.stderr.write("\tpython prepare.py data-file\n")
+        sys.exit(1)
+
+    data_path = sys.argv[1]
+
+    if not os.path.exists(data_path):
+        sys.stderr.write("PATH_TRAIN is not exist!\n")
+        sys.exit(1)
+
+    # path for outputs
+    output_dir = './models/'
+    os.makedirs(output_dir, exist_ok=True)
+
+    train_model(data_path, alpha, output_dir)
+
+
+if __name__ == "__main__":
+    main()
